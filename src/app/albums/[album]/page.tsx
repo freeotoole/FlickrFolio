41b10042395418846b270dcea29b90d7dff803cc
Free <14006733+freeotoole@users.noreply.github.com>
2024-03-22 10:41:21 +1100

    feat: closes #8 - Add active states to global nav

--- src/app/albums/[album]/page.tsx
+++ src/app/albums/[album]/page.tsx
@@ -1,14 +1,17 @@
 import { fetchPhotoset, fetchPhotosetInfo } from '@/app/apiUtils'
 import Gallery from '@/app/components/Gallery'
 import GlobalSidebar from '@/app/components/GlobalSidebar'
+import { settings } from '@/app/settings'
 
 interface Params {
   album: string
 }
 
 export default async function AlbumsPage({ params }: { params: Params }) {
-  const album = await fetchPhotoset(params.album)
-  const info = await fetchPhotosetInfo(params.album)
+  // get album id from settings
+  const albumId = settings.albums[params.album]?.id
+  const album = await fetchPhotoset(albumId)
+  const info = await fetchPhotosetInfo(albumId)
 
   if (!album) {
     return <div>Loading...</div>
@@ -20,8 +23,8 @@ export default async function AlbumsPage({ params }: { params: Params }) {
         description={info?.photoset?.description?._content}
       />
       <div className="relative items-start justify-items-start py-4">
-        {<Gallery photos={album?.photoset?.photo} album={params.album} />}
-        {/* {JSON.stringify(info)} */}
+        {/* {JSON.stringify(settings.albums)} */}
+        {<Gallery photos={album?.photoset?.photo} album={albumId} />}
       </div>
     </div>
   )
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,9 @@
+import path from 'path'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { settings } from '../settings'
 
 type Layout = 'horizontal' | 'vertical'
 
@@ -9,6 +13,11 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ layout }) => {
+  const pathname = usePathname()
+  // TODO: Possibly refactor to create path array and test position
+  const isActive = (slug: string) => {
+    return pathname.includes(slug)
+  }
   return (
     <nav className={`navbar flex-1 ${layout}`}>
       <div className="min-h-[50vh] text-right">
@@ -19,28 +28,23 @@ const Navbar: React.FC<NavbarProps> = ({ layout }) => {
         <nav className="w-full">
           <ul className="">
             <li>
-              <Link className="block py-2 font-semibold" href="/photos">
+              <Link
+                className={`block py-2 hover:font-semibold ${isActive('/photos') ? 'font-semibold' : ''}`}
+                href="/photos"
+              >
                 Show Latest
               </Link>
             </li>
-            <li>
-              <Link
-                className="block py-2 hover:font-semibold"
-                href="/albums/72177720313681808"
-              >
-                Film
-              </Link>
-            </li>
-            <li>
-              <Link className="block py-2 hover:font-semibold" href="#">
-                Album Two
-              </Link>
-            </li>
-            <li>
-              <Link className="block py-2 hover:font-semibold" href="#">
-                Album Three
-              </Link>
-            </li>
+            {Object.values(settings.albums).map((album, i) => (
+              <li key={i}>
+                <Link
+                  className={`block py-2 hover:font-semibold ${isActive(`/albums/${album.slug}`) ? 'font-semibold' : ''}`}
+                  href={`/albums/${album.slug}`}
+                >
+                  {album.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
